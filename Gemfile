d961ed5116b739b9dadbf4341c492fe75d6b4b84
Mike Torres <apolloproductionsdj@gmail.com>
2020-12-02 00:29:34 -0700

    fixed uploading of picture by image_tag gram.picture.url

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'bootstrap', '~> 5.0.0.alpha3'
 gem 'jquery-rails'
 gem 'simple_form'
 gem 'devise'
+gem 'carrierwave', '~> 2.0' 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_042901) do
+ActiveRecord::Schema.define(version: 2020_12_01_002543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_042901) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "picture"
     t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
